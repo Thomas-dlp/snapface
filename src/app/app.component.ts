fefbc7e8216b2622f4183ec44ea5d8199fcfb7a2
Thomas <tomdelaplanche@gmail.com>
2025-01-30 19:00:55 +0100

    cours 2 partie 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { filter, interval, map, Observable, take, tap } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { CommonModule, NgIf } from '@angular/common';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { FaceSnapService } from './services/face-snaps.service';
+import { FaceSnap } from './models/face-snap';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
     RouterOutlet,
+    CommonModule
+   
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
- 
+export class AppComponent implements OnInit {
 
+  faceSnaps!: FaceSnap[]
+constructor(private faceSnapServices:FaceSnapService){}
+
+
+  ngOnInit(): void {
+   this.faceSnaps=this.faceSnapServices.getFaceSnaps();
+interval(1000).pipe(
+  take(3),
+  tap(console.log),
+).subscribe();
+  }
+ 
+logger(text:string){
+  console.log(`Log: ${text}`);
+}
 }
